be048425b66fd812300b70d8ac421f698e45e738
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-04-13 09:28:12 -0700

    socket updated

--- server.js
+++ server.js
@@ -3,28 +3,60 @@ import router from "./router/routes.js";
 import connect from "./db/conn.js";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const connectedUsers = {};
+
+app.use(cors());
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  const userId = socket.handshake.query.userId;
+  console.log(`User`, socket.id, ` connected with id: ${userId}`);
+
+  if (!connectedUsers[userId]) {
+    connectedUsers[userId] = [socket.id]; // create a new array for this user's sockets
+  } else {
+    connectedUsers[userId].push(socket.id); // add the socket to the existing array for this user
+  }
+  console.log(connectedUsers);
 
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    const index = connectedUsers[userId].indexOf(socket.id);
+    if (index !== -1) {
+      connectedUsers[userId].splice(index, 1);
+    }
+    console.log(socket.id, "user disconnected");
+    console.log(connectedUsers);
+  });
+
+  socket.on("send", (data) => {
+    // Do something with the data, like broadcasting it to other connected clients
+    console.log(data);
+    const userId = data.userId; // Replace with the user ID you want to send the message to
+    console.log(userId);
+    const socketId = connectedUsers[userId];
+    console.log(socketId);
+    const otherClients = connectedUsers[userId].filter(
+      (element) => element !== socket.id
+    );
+    console.log(otherClients);
+
+    if (connectedUsers[userId].length < 2) {
+      console.log(`User ${userId} does not have at least 2 clients connected`);
+    } else {
+      io.to(otherClients).emit("get", data.message);
+    }
+    console.log(data.userId);
   });
 });
-
-io.on("message", (data) => {
-  console.log(`Received message: ${data}`);
-});
-
+ 
 app.use(express.json());
 const port = 3000;
 
 app.get("/", (req, res) => {
-  io.emit("message", "Hello, server!");
   res.send({
     message: " The API is running sucessfully",
   });
