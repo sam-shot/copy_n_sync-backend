27f896772f09fb4bd9a8a27a6ea1f9a1ecfc3ca2
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-07-02 03:54:51 +0100

    Update server.js to allow cors

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
   next();
 });
 
