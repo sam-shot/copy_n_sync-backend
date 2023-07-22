12c6d454a3274fbe35d2766d3d3871d5c416b133
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-07-22 22:23:55 +0100

    feat : Upload file endpoint implemented

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import { instrument } from "@socket.io/admin-ui";
+import axios from "axios";
 import cors from "cors";
 import express from "express";
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import http from "http";
+import multer from "multer";
 import { Server } from "socket.io";
 import connect from "./db/conn.js";
 import text_model from "./model/text_model.js";
 import user_model from "./model/user_model.js";
 import router from "./router/routes.js";
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 const app = express();
 app.use(cors());
@@ -18,6 +24,20 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBE3WoR8yrVhKRHFus_QoV4oSCz5-2n4k",
+  authDomain: "copy-n-sync.firebaseapp.com",
+  projectId: "copy-n-sync",
+  storageBucket: "copy-n-sync.appspot.com",
+  messagingSenderId: "818553963570",
+  appId: "1:818553963570:web:b287c0fb68fe258d49282b",
+  measurementId: "G-C7YCG5H12D",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp, "gs://copy-n-sync.appspot.com/");
 
 const connectedUsers = {};
 
@@ -58,12 +78,11 @@ io.on("connection", (socket) => {
         "error",
         "You need to have at least 2 devices for Text Syncing"
       );
-     
+
       console.log(`User ${userId} does not have at least 2 clients connected`);
     } else {
-      
       io.to(otherClients).emit("get", data.message);
-       if (!data.fromHistory) {
+      if (!data.fromHistory) {
         const newText = new text_model({
           text: data.message,
           user: userId,
@@ -90,6 +109,38 @@ app.get("/", (req, res) => {
   res.send({
     message: " The API is running sucessfully",
   });
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  const {userId} = req.body;
+  // const userId = "642f3ad88c7ed964295810f5";
+  const storageRef = ref(storage, req.file.originalname);
+  user_model
+    .findById(userId)
+    .then((result) => {
+      uploadBytes(storageRef, req.file.buffer).then(async (snapshot) => {
+        const link = await getDownloadURL(snapshot.ref);
+        const data = {
+          text: link,
+
+          userId: userId,
+        };
+        await axios
+          .post("https://copy-n-sync-backend.vercel.app/send/text", data)
+          .then((response) => {
+            res.status(200).send(response.data);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
+    })
+    .catch((error) => {
+      return res.status(404).send({
+        message: "Cant Find User",
+        status: "404",
+      });
+    });
 });
 
 app.use("/", router);
